20efa02a775485bb8833e57503a2dd74bb02399a
omprakakash39 <omprakakash39@gmail.com>
2026-08-24 23:44:08 +0530

    Refactor EggGUI to simplify inventory title and lore
    Replaced Component text with string for inventory title and updated item lore for clarity.

--- src/main/java/com/sayan/sparkpets/gui/EggGUI.java
+++ src/main/java/com/sayan/sparkpets/gui/EggGUI.java
@@ -3,8 +3,6 @@ package com.sayan.sparkpets.gui;
 import com.sayan.sparkpets.SparkPets;
 import com.sayan.sparkpets.models.Rarity;
 import com.sayan.sparkpets.utils.ItemBuilder;
-import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
@@ -22,26 +20,18 @@ public class EggGUI {
     }
 
     public void open() {
-        Inventory inv = Bukkit.createInventory(null, 27, Component.text("Pet Eggs")
-                .color(NamedTextColor.AQUA));
+        Inventory inv = Bukkit.createInventory(null, 27, "§bPet Eggs");
 
-        // 4 Eggs
         inv.setItem(10, plugin.getPetManager().createPetEgg(Rarity.REGULAR));
         inv.setItem(12, plugin.getPetManager().createPetEgg(Rarity.GOLD));
         inv.setItem(14, plugin.getPetManager().createPetEgg(Rarity.RAINBOW));
         inv.setItem(16, plugin.getPetManager().createPetEgg(Rarity.SHINY));
 
-        // Info
         inv.setItem(4, new ItemBuilder(Material.BOOK)
-                .name("§b§lPet Eggs Info")
-                .lore(
-                        "§7Only Admins can take these eggs.",
-                        "§7Normal players can only view them.",
-                        "",
-                        "§7Right-click an egg in inventory to open it!"
-                ).build());
+                .name("§b§lPet Eggs")
+                .lore("§7Only admins can take these.", "§7Right-click egg in inventory to open.")
+                .build());
 
-        // Fill
         ItemStack glass = new ItemBuilder(Material.LIGHT_BLUE_STAINED_GLASS_PANE).name(" ").build();
         for (int i = 0; i < 27; i++) {
             if (inv.getItem(i) == null) inv.setItem(i, glass);
